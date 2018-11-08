098c1e48ba4437d10e1ed14d30ad1c3d26a3a1e7
gracekoester <grace.koester@cuw.edu>
2018-11-08 11:56:29 -0600

    //add

--- app/src/main/java/com/example/aweso/arminterpreter/MainActivity.java
+++ app/src/main/java/com/example/aweso/arminterpreter/MainActivity.java
@@ -11,12 +11,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ARMap.lookupInstruction("ADD").display();
+        ARMap.init();
+        //ARMap.lookupInstruction("ADD").display();
     }
 
     public void registersButtonPressed(View v)
     {
         Intent i = new Intent(this, RegisterScreen.class);
         this.startActivity(i);
+    }
+
+    private String addRegisters(String bin, String bin2)
+    {
+        return Integer.toBinaryString(Integer.parseInt(bin, 2) +
+                Integer.toBinaryString(Integer.parseInt(bin2, 2)));
+    }
+
+
+    public void executeButtonPressed(View v)
+    {
+        //Read the instruction from the instructionET and appropriately
+        //execute it
+        //For example, if instructionET contained: ADD X0, X1, X2
+        //Your code should grab the value from X1 and X2, add them
+        //together, and store the result in X0
+
+        instructionET = (EditText)findViewById(R.id.instructionET);
+
+
+        Register 1 = this.registerET[1];
+        Register 2 = this.registerET[2];
+
+        String register1bin = BinaryValue(Register 1);
+        String register2bin = BinaryValue(Register 2);
+
+        String answer = addRegisters(register1bin, register2bin);
+
+        this.theRegisterETs[0].setText("" + ARMap.registers[i].getValue());
+
+
     }
 }
